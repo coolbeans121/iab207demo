8fad74d5663647739a790ba184a4b98898f09bb2
fdav <166580733+coolbeans121@users.noreply.github.com>
2025-05-29 14:18:36 +1000

    Update views.py

--- travel/views.py
+++ travel/views.py
@@ -1,1 +1,26 @@
+from flask import Blueprint, render_template
+#task1 wk12
+from flask import request, redirect, url_for
+from .models import Destination
+
+from . import db
+
+mainbp = Blueprint('main', __name__)
+
+#task 1 wk12
+@mainbp.route('/')
+def index():
+    destinations = db.session.scalars(db.select(Destination)).all()    
+    return render_template('index.html', destinations=destinations)
+
+@mainbp.route('/search')
+def search():
+    #task 1 wk 12
+    if request.args['search'] and request.args['search'] != "":
+        print(request.args['search'])
+        query = "%" + request.args['search'] + "%"
+        destinations = db.session.scalars(db.select(Destination).where(Destination.description.like(query)))
+        return render_template('index.html', destinations=destinations)
+    else:
+        return redirect(url_for('main.index'))
 
